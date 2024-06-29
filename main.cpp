b4b6f785f64238160c846f649b1c4f7a4bbe1545
Lucas <lucasktam@gmail.com>
2024-06-29 15:53:03 -0700

    added stuff to main.cpp
    printing some fun stuff

--- main.cpp
+++ main.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main(){
     cout << "Hello world" << endl;
+    int rows = 5;
+    int cols = 4;
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            cout << ":3 ";
+        }
+        cout << "\n";
+    }
+    
     return 0;
 }
